25a9ea5a430bbadaa626b0f7b3a051d9a3207c69
flauntingspade4 <elliotwhybrow@gmail.com>
2021-01-31 21:17:38 +0000

    `deserialize_byte_buf` is now implemented fully

--- src/de.rs
+++ src/de.rs
@@ -316,14 +316,32 @@ impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
                 None => Err(DeBencodingError::NoFoundColon),
             }
         }?;
-        visitor.visit_bytes(s.as_ref())
+        visitor.visit_borrowed_bytes(s)
     }
 
-    fn deserialize_byte_buf<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        unimplemented!()
+        let s = {
+            match self.input.iter().position(|b| b == &b':') {
+                Some(count) => {
+                    let (length, rest_of) = self.input.split_at(count);
+                    let len = match std::str::from_utf8(length).unwrap().parse::<usize>() {
+                        // Add one to help ignore the added space from ':'
+                        Ok(t) => t + 1,
+                        Err(_) => return Err(DeBencodingError::ParseIntError),
+                    };
+                    // Make to_return between the colon, and the end of the second string
+                    let to_return = &rest_of[1..len];
+                    // Remove the used string
+                    self.input = &self.input[count + len..];
+                    Ok(to_return)
+                }
+                None => Err(DeBencodingError::NoFoundColon),
+            }
+        }?;
+        visitor.visit_borrowed_bytes(s)
     }
 
     // An absent optional is represented as the JSON `null` and a present
